817ecb522cd89ebeb3127b711c0b4e36c89c5110
Bob Clough <bob@clough.me>
2014-07-01 15:04:59 +0100

    Added version 1 of mqtt 2 sound script

---
+++ README.md
@@ -0,0 +1,6 @@
+mqtt-2-bot
+==========
+
+This is a python script that connects our MQTT network at Hackspace Manchester to our pycat IRC bot.
+
+You can see it working at irc://irc.freenode.net/#hacman
---
+++ mqtt-2-sound.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import mosquitto
+import yaml
+import pygame
+import os
+
+config_f = open('config.yaml')
+config = yaml.safe_load(config_f)
+config_f.close()
+
+# set up the mixer at 44100 frequency, with 16 signed bits per sample, 1 channel, with a 2048 sample buffer
+pygame.mixer.init(44100, -16, 1, 2048)
+# set volume to maximum
+pygame.mixer.music.set_volume(1.0)
+
+currently_playing_file = ""
+
+
+def on_message(mosq, obj, msg):
+    if msg.topic == 'door/inner/opened/username':
+        play("audio/%s_announce.ogg" % msg.payload)
+    elif msg.topic == 'door/outer/buzzer':
+        play("audio/buzzer.ogg")
+    elif msg.topic == 'door/outer/opened/username':
+        play("audio/outer_door_opened.ogg")
+    elif msg.topic == 'door/inner/doorbell':
+        play("audio/doorbell.ogg")
+
+
+def play(filename):
+    global currently_playing_file
+    if os.path.isfile(filename):
+        if (not pygame.mixer.music.get_busy()) or (currently_playing_file is not filename):
+            currently_playing_file = filename
+            pygame.mixer.music.load(filename)
+            pygame.mixer.music.play()
+
+
+mqttc = mosquitto.Mosquitto(config['mqtt']['name'])
+mqttc.connect(config['mqtt']['server'], 1883, 60, True)
+
+mqttc.subscribe("door/outer/buzzer")
+mqttc.subscribe("door/outer/opened/username")
+
+mqttc.subscribe("door/inner/doorbell")
+mqttc.subscribe("door/inner/opened/username")
+mqttc.on_message = on_message
+
+while mqttc.loop() == 0:
+    pass
+
